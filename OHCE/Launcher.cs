c22488e600150a17d55449e37674df6cd45bf133
Alexis Def <def.alexis@outlook.fr>
2023-01-31 01:10:53 +0100

    Ajout d'un launcher = interaction avec la console

---
+++ OHCE/Launcher.cs
@@ -0,0 +1,29 @@
+﻿using OHCE;
+using OHCE.Langues;
+using System.Globalization;
+
+ILangue langueSys = new LangueSystème().RecupLangue();
+string periodeSys = new PeriodeSystème().RecupererPeriode(langueSys);
+string chaine;
+var ohce = new OHCEBuilder().withLangue(langueSys).withPeriode(periodeSys).build();
+
+Console.WriteLine(ohce.DireBonjour());
+
+Console.WriteLine("");
+
+do
+{
+    Console.Write(langueSys.Saisie + " : ");
+    chaine = Console.ReadLine();
+
+    if (chaine != "")
+    {
+        Console.WriteLine("");
+        Console.WriteLine(ohce.Miroir(chaine));
+        Console.WriteLine("");
+    }
+
+} while (chaine != "");
+
+Console.WriteLine("");
+Console.WriteLine(ohce.DireAuRevoir());
